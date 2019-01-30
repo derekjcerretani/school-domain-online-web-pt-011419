29f464f05ae2fb7f0454a8352bd8b6072b385371
Derek Cerretani <derekcerretani@gmail.com>
2019-01-30 16:10:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,12 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
-    #@roster => {name => [grade]}
+    if @roster[grade] == nil
+      @roster[grade] = name
+    else
+      @roster[grade] << name
+    end
+  end
+
   end
 end
